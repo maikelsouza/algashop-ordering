bea487fa012db779cb1166de27643a8f7cf79e76
RE045908 <re045908@qintess.com>
2025-08-05 11:24:33 -0300

    Add news tests and new classes about validations and error messages

--- src/test/java/com/algaworks/algashop/ordering/domain/entity/CustomerTest.java
+++ src/test/java/com/algaworks/algashop/ordering/domain/entity/CustomerTest.java
@@ -1,28 +1,50 @@
 package com.algaworks.algashop.ordering.domain.entity;
 
 import com.algaworks.algashop.ordering.domain.utility.IdGenerator;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
-import java.util.UUID;
 
 public class CustomerTest {
 
-    @Test
-    public void testingCustomer(){
-        Customer customer = new Customer(
-                IdGenerator.generateTimeBasedUUID(),
-                "Jhon Doe",
-                LocalDate.of(1991, 7, 5),
-                "Jhon.doe@gmail.com",
-                "478-256-2504",
-                "255-08-0578",
-                true,
-                OffsetDateTime.now()
-        );
 
-        customer.addLoyaltyPoints(10);
+    @Test
+    void given_invalidEmail_whenTryCreateCustomer_shouldGenerateException() {
+
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() ->{
+                    new Customer(
+                        IdGenerator.generateTimeBasedUUID(),
+                        "Jhon Doe",
+                        LocalDate.of(1991, 7, 5),
+                        "invalid",
+                        "478-256-2504",
+                        "255-08-0578",
+                        false,
+                        OffsetDateTime.now()
+                    );
+                });
+    }
+
+    @Test
+    void given_invalidEmail_whenTryUpdatedCustomerEmail_shouldGenerateException() {
+            Customer customer = new Customer(
+                    IdGenerator.generateTimeBasedUUID(),
+                    "John Doe",
+                    LocalDate.of(1991, 7, 5),
+                    "john.doe@gmail.com",
+                    "478-256-2504",
+                    "255-08-0578",
+                    false,
+                    OffsetDateTime.now()
+            );
+
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() ->{
+                    customer.changeEmail("invalid");
+                });
     }
 
 }
