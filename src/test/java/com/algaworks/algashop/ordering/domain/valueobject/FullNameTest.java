c4d1ac1447cadde4aa82bb3541f4aab2a0134fc6
RE045908 <re045908@qintess.com>
2025-08-14 15:39:59 -0300

    SMS 6.12 Challenge: Implementing Value Objects - FullName test

---
+++ src/test/java/com/algaworks/algashop/ordering/domain/valueobject/FullNameTest.java
@@ -0,0 +1,51 @@
+package com.algaworks.algashop.ordering.domain.valueobject;
+
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static com.algaworks.algashop.ordering.domain.exception.ErrorMessages.*;
+
+
+class FullNameTest {
+
+    @Test
+    void shouldNoCreateFirstNameNull(){
+        Assertions.assertThatExceptionOfType(NullPointerException.class)
+                .isThrownBy(() -> new FullName(null, "name"))
+                .withMessage(VALIDATION_ERROR_FULLNAME_FIRST_NAME_IS_NULL);
+    }
+
+    @Test
+    void shouldNoCreateLastNameNull(){
+        Assertions.assertThatExceptionOfType(NullPointerException.class)
+                .isThrownBy(() -> new FullName("name", null))
+                .withMessage(VALIDATION_ERROR_FULLNAME_LAST_NAME_IS_NULL);
+    }
+
+    @Test
+    void shouldNoCreateFirstNameBlank(){
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> new FullName("", "name"))
+                .withMessage(VALIDATION_ERROR_FULLNAME_FIRST_NAME_IS_BLANK);
+    }
+
+    @Test
+    void shouldNoCreateLastNameBlank(){
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> new FullName("name", ""))
+                .withMessage(VALIDATION_ERROR_FULLNAME_LAST_NAME_IS_BLANK);
+    }
+
+    @Test
+    void shoulDCreateFullName(){
+        var fullName = new FullName("John", "Peter");
+
+        Assertions.assertThat(fullName.toString()).isEqualTo("John Peter");
+
+    }
+
+
+
+}
