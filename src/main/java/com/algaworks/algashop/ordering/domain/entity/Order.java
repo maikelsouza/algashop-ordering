c31d7a33382c170fff7d510341aaaeaf8b4ee60d
RE045908 <re045908@qintess.com>
2025-08-24 11:33:30 -0300

    SMS level 2 - 6.35 - Challenge: Canceling an Order

--- src/main/java/com/algaworks/algashop/ordering/domain/entity/Order.java
+++ src/main/java/com/algaworks/algashop/ordering/domain/entity/Order.java
@@ -126,6 +126,11 @@ public class Order {
         this.setReadyAt(OffsetDateTime.now());
     }
 
+    public void cancel(){
+        this.changeStatus(OrderStatus.CANCELED);
+        this.setCanceledAt(OffsetDateTime.now());
+    }
+
     public void changePaymentMethod(PaymentMethod paymentMethod){
         this.verifyIfChangeable();
         Objects.requireNonNull(paymentMethod);
@@ -168,7 +173,10 @@ public class Order {
     }
 
     public boolean isPaid(){ return OrderStatus.PAID.equals(this.status());}
+
     public boolean isReady(){ return OrderStatus.READY.equals(this.status());}
+
+    public boolean isCanceled(){ return OrderStatus.CANCELED.equals(this.status());}
 
     public OrderId id() {
         return id;
@@ -308,9 +316,7 @@ public class Order {
         this.canceledAt = canceledAt;
     }
 
-    private void setReadyAt(OffsetDateTime readyAt) {
-        this.readyAt = readyAt;
-    }
+    private void setReadyAt(OffsetDateTime readyAt) {this.readyAt = readyAt;}
 
     private void setBilling(Billing billing) {
         this.billing = billing;
