5aa2f851add14b8b555b4d591de40454138fcbc3
RE045908 <re045908@qintess.com>
2025-08-27 14:02:59 -0300

    SMS level 2 - 8.13 - Disassembler: Jakarta Persistence Entity to Domain Entity Converter

--- src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepositoryIT.java
+++ src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepositoryIT.java
@@ -1,15 +1,12 @@
 package com.algaworks.algashop.ordering.infrastructure.persistence.repository;
 
-import com.algaworks.algashop.ordering.domain.model.utility.IdGenerator;
 import com.algaworks.algashop.ordering.infrastructure.persistence.entity.OrderPersistenceEntity;
+import com.algaworks.algashop.ordering.infrastructure.persistence.entity.OrderPersistenceEntityTestDataBuilder;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-import java.math.BigDecimal;
-import java.time.OffsetDateTime;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -25,21 +22,9 @@ class OrderPersistenceEntityRepositoryIT {
 
     @Test
     public void shouldPersist(){
-
-        long id = IdGenerator.generateTSID().toLong();
-        OrderPersistenceEntity entity = OrderPersistenceEntity.builder()
-                .id(id)
-                .customerId(IdGenerator.generateTimeBasedUUID())
-                .totalItems(2)
-                .totalAmount(new BigDecimal(1000))
-                .status("DRAFT")
-                .paymentMethod("CREDIT_CARD")
-                .placedAt(OffsetDateTime.now())
-                .build();
-
+        OrderPersistenceEntity entity = OrderPersistenceEntityTestDataBuilder.existingOrder().build();
         orderPersistenceEntityRepository.saveAndFlush(entity);
-
-        Assertions.assertThat(orderPersistenceEntityRepository.existsById(id)).isTrue();
+        Assertions.assertThat(orderPersistenceEntityRepository.existsById(entity.getId())).isTrue();
     }
 
     @Test
