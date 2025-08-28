71947cf8b84c903571026044f8ba28db6e35e5a5
RE045908 <re045908@qintess.com>
2025-08-28 09:23:52 -0300

    SMS level 2 - 8.23. Exercise: Copying Data from Value Objects to Embeddables

--- src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/assembler/OrderPersistenceEntityAssembler.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/assembler/OrderPersistenceEntityAssembler.java
@@ -1,8 +1,18 @@
 package com.algaworks.algashop.ordering.infrastructure.persistence.assembler;
 
 import com.algaworks.algashop.ordering.domain.model.entity.Order;
+import com.algaworks.algashop.ordering.domain.model.valueobject.Address;
+import com.algaworks.algashop.ordering.domain.model.valueobject.Billing;
+import com.algaworks.algashop.ordering.domain.model.valueobject.Recipient;
+import com.algaworks.algashop.ordering.domain.model.valueobject.Shipping;
+import com.algaworks.algashop.ordering.infrastructure.persistence.embeddable.AddressEmbeddable;
+import com.algaworks.algashop.ordering.infrastructure.persistence.embeddable.BillingEmbeddable;
+import com.algaworks.algashop.ordering.infrastructure.persistence.embeddable.RecipientEmbeddable;
+import com.algaworks.algashop.ordering.infrastructure.persistence.embeddable.ShippingEmbeddable;
 import com.algaworks.algashop.ordering.infrastructure.persistence.entity.OrderPersistenceEntity;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 @Component
 public class OrderPersistenceEntityAssembler{
@@ -23,6 +33,55 @@ public class OrderPersistenceEntityAssembler{
         orderPersistenceEntity.setCancelAt(order.canceledAt());
         orderPersistenceEntity.setReadyAt(order.readyAt());
         orderPersistenceEntity.setVersion(order.version());
+        orderPersistenceEntity.setBilling(buildBilling(order.billing()));
+        orderPersistenceEntity.setShipping(buildShipping(order.shipping()));
+
         return orderPersistenceEntity;
+    }
+
+    private BillingEmbeddable buildBilling(Billing billing) {
+        Objects.requireNonNull(billing);
+        return BillingEmbeddable.builder()
+                .firstName(billing.fullName().firstName())
+                .lastName(billing.fullName().lastName())
+                .document(billing.document().value())
+                .phone(billing.phone().value())
+                .email(billing.email().value())
+                .address(buildAddress(billing.address()))
+                .build();
+    }
+
+    private AddressEmbeddable buildAddress(Address address){
+        Objects.requireNonNull(address);
+        return AddressEmbeddable.builder()
+                .state(address.state())
+                .city(address.city())
+                .neighborhood(address.neighborhood())
+                .complement(address.complement())
+                .number(address.number())
+                .street(address.street())
+                .zipCode(address.zipCode().value())
+                .build();
+    }
+
+    private ShippingEmbeddable buildShipping(Shipping shipping){
+        Objects.requireNonNull(shipping);
+        return ShippingEmbeddable.builder()
+                .cost(shipping.cost().value())
+                .address(buildAddress(shipping.address()))
+                .expectedDate(shipping.expectedDate())
+                .recipient(buildRecipient(shipping.recipient()))
+                .build();
+
+    }
+
+    private RecipientEmbeddable buildRecipient(Recipient recipient) {
+        Objects.requireNonNull(recipient);
+        return RecipientEmbeddable.builder()
+                .document(recipient.document().value())
+                .firstName(recipient.fullName().firstName())
+                .lastName(recipient.fullName().lastName())
+                .phone(recipient.phone().value())
+                .build();
     }
 }
