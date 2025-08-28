ffd6bd01991748c79260865d7073529aedccbd50
RE045908 <re045908@qintess.com>
2025-08-28 15:38:15 -0300

    SMS level 2 - 8.29 - Lazy Loading Problem

--- src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/provider/OrdersPersistenceProviderIT.java
+++ src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/provider/OrdersPersistenceProviderIT.java
@@ -13,8 +13,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
-
-import java.util.Optional;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @DataJpaTest
@@ -54,6 +54,16 @@ class OrdersPersistenceProviderIT {
         Assertions.assertThat(persistenceEntity.getCreatedByUserId()).isNotNull();
         Assertions.assertThat(persistenceEntity.getLastModifiedAt()).isNotNull();
         Assertions.assertThat(persistenceEntity.getLastModifiedByUserId()).isNotNull();
+    }
+
+    @Test
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
+    public void shouldAddFindNotFailWhenNotTransaction(){
+        Order order = OrderTestDataBuilder.anOrder().build();
+        persistenceProvider.add(order);
+
+        Assertions.assertThatNoException()
+                .isThrownBy(() -> persistenceProvider.ofId(order.id()).orElseThrow());
 
     }
 }
