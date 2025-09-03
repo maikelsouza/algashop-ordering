10f01c562f7495cc7561e4ab699d31ae410fd8a7
RE045908 <re045908@qintess.com>
2025-09-03 14:34:37 -0300

    Removed Imports not used

--- src/test/java/com/algaworks/algashop/ordering/domain/model/service/CustomerRegistrationServiceIT.java
+++ src/test/java/com/algaworks/algashop/ordering/domain/model/service/CustomerRegistrationServiceIT.java
@@ -1,16 +1,9 @@
 package com.algaworks.algashop.ordering.domain.model.service;
 
 import com.algaworks.algashop.ordering.domain.model.entity.Customer;
-import com.algaworks.algashop.ordering.domain.model.repository.Customers;
 import com.algaworks.algashop.ordering.domain.model.valueobject.*;
-import com.algaworks.algashop.ordering.domain.model.valueobject.id.CustomerId;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
