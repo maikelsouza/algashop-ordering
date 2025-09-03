8c5342f80dfb7b3d49ea4a909a34cd6888d30535
RE045908 <re045908@qintess.com>
2025-09-03 17:18:52 -0300

    SMS level 2 - 9.10 - Challenge - Implement BuyNow Domain Service

--- src/main/java/com/algaworks/algashop/ordering/domain/model/service/BuyNowService.java
+++ src/main/java/com/algaworks/algashop/ordering/domain/model/service/BuyNowService.java
@@ -12,24 +12,6 @@ import com.algaworks.algashop.ordering.domain.model.valueobject.id.CustomerId;
 @DomainService
 public class BuyNowService {
 
-
-    public Order buyNow2(Product product, CustomerId customerId,
-                        Billing billing, Shipping shipping,
-                        Quantity quantity, PaymentMethod paymentMethod){
-
-        product.checkOutOfStock();
-
-        Order order = Order.draft(customerId);
-        order.changeBilling(billing);
-        order.changeShipping(shipping);
-        order.changePaymentMethod(paymentMethod);
-
-        order.addItem(product, quantity);
-        order.place();
-        return order;
-    }
-
-
     public Order buyNow(Product product,
                         CustomerId customerId,
                         Billing billing,
