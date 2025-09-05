03724df6848f7877cc400020d49a528a11f4001b
RE045908 <re045908@qintess.com>
2025-09-05 14:41:42 -0300

    SMS level 2 - 12.8 - Implementing instant purchase use case

--- src/main/java/com/algaworks/algashop/ordering/domain/model/ErrorMessages.java
+++ src/main/java/com/algaworks/algashop/ordering/domain/model/ErrorMessages.java
@@ -38,6 +38,7 @@ public class ErrorMessages {
     public static final String ERROR_ORDER_NOT_BELONGS_TO_CUSTOMER = "Order %s not belongs to customer %s";
     public static final String ERROR_ORDER_CANNOT_BE_EDITED = "Order %s with status %s cannot be edited";
     public static final String ERROR_PRODUCT_IS_OUT_OF_STOCK = "Product %s is out of stock";
+    public static final String ERROR_PRODUCT_NOT_FOUND = "Product %s not found";
     public static final String ERROR_SHOPPING_CARD_DOES_NOT_CONTAIN_ITEM = "Shopping Card %s does not contain item %s";
     public static final String ERROR_SHOPPING_CARD_DOES_NOT_CONTAIN_PRODUCT = "Shopping Card %s does not contain product %s";
     public static final String ERROR_SHOPPING_CARD_ITEM_INCOMPATIBLE_PRODUCT = "Shopping Card Item %s incompatible product %s";
