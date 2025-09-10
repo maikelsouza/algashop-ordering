4baaf1d776049f26c8a4795d9c7b08d259eeff15
RE045908 <re045908@qintess.com>
2025-09-10 17:19:59 -0300

    SMS level 2 - 13.6 - Listening to events with EventListener

---
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/listener/customer/CustomerEventListener.java
@@ -0,0 +1,27 @@
+package com.algaworks.algashop.ordering.infrastructure.listener.customer;
+
+import com.algaworks.algashop.ordering.domain.model.customer.CustomerArchivedEvent;
+import com.algaworks.algashop.ordering.domain.model.customer.CustomerRegisteredEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class CustomerEventListener {
+
+    @EventListener
+    public void listen(CustomerRegisteredEvent event){
+        log.info("CustomerRegisteredEvent listen 1");
+    }
+
+    @EventListener
+    public void listen2(CustomerRegisteredEvent event){
+        log.info("CustomerRegisteredEvent listen 2");
+    }
+
+    @EventListener
+    public void listen(CustomerArchivedEvent event){
+        log.info("CustomerArchivedEvent listen 1");
+    }
+}
