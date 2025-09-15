3cb803258af64a88831ecb31d7b2d66510a5324d
RE045908 <re045908@qintess.com>
2025-09-15 17:22:26 -0300

    SMS level 2 - 15.4 - Simplified implementation with Entity Manager

--- src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/customer/CustomerPersistenceEntityRepository.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/customer/CustomerPersistenceEntityRepository.java
@@ -5,8 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface CustomerPersistenceEntityRepository extends JpaRepository<CustomerPersistenceEntity, UUID>,
-                                                             CustomerPersistenceEntityQueries{
+public interface CustomerPersistenceEntityRepository extends JpaRepository<CustomerPersistenceEntity, UUID>{
 
     Optional<CustomerPersistenceEntity> findByEmail(String value);
 
--- src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/customer/CustomerQueryServiceImpl.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/customer/CustomerQueryServiceImpl.java
@@ -2,30 +2,61 @@ package com.algaworks.algashop.ordering.infrastructure.persistence.customer;
 
 import com.algaworks.algashop.ordering.application.customer.query.CustomerOutput;
 import com.algaworks.algashop.ordering.application.customer.query.CustomerQueryService;
-import com.algaworks.algashop.ordering.application.utility.Mapper;
 import com.algaworks.algashop.ordering.domain.model.customer.CustomerId;
 import com.algaworks.algashop.ordering.domain.model.customer.CustomerNotFoundException;
-import com.algaworks.algashop.ordering.domain.model.customer.Customers;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.TypedQuery;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Objects;
 import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CustomerQueryServiceImpl implements CustomerQueryService {
 
-    private final Mapper mapper;
+    private final EntityManager entityManager;
 
-    private final Customers customers;
-
-    private final CustomerPersistenceEntityRepository repository;
+    private static final String findByIdAsOutputJPQL = """                      
+            SELECT new com.algaworks.algashop.ordering.application.customer.query.CustomerOutput(
+                c.id,
+                c.firstName,
+                c.lastName,
+                c.email,
+                c.document,
+                c.phone,
+                c.birthDate,
+                c.loyaltyPoints,
+                c.registeredAt,
+                c.archivedAt,
+                c.promotionNotificationsAllowed,
+                c.archived,
+                new com.algaworks.algashop.ordering.application.commons.AddressData(
+                    c.address.street,
+                    c.address.number,
+                    c.address.complement,
+                    c.address.neighborhood,
+                    c.address.city,
+                    c.address.state,
+                    c.address.zipCode
+                )
+            )
+            FROM CustomerPersistenceEntity c
+            WHERE c.id = :id""";
 
     @Override
     public CustomerOutput findById(UUID customerId) {
         Objects.requireNonNull(customerId);
-        return repository.findByIdAsOutput(customerId)
-                        .orElseThrow(() -> new CustomerNotFoundException(new CustomerId(customerId)));
+        try {
+            TypedQuery<CustomerOutput> query = entityManager.createQuery(findByIdAsOutputJPQL, CustomerOutput.class);
+            query.setParameter("id", customerId);
+            return query.getSingleResult();
+        } catch (NoResultException e){
+            throw new CustomerNotFoundException(new CustomerId(customerId));
+        }
     }
 }
