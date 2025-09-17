ada20b5f19a42ef0d87ed2dec853d57b4662fb50
RE045908 <re045908@qintess.com>
2025-09-17 13:32:48 -0300

    SMS level 2 - 15.11 - Implementing sorting

--- src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/order/OrderQueryServiceImpl.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/order/OrderQueryServiceImpl.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -56,14 +57,6 @@ public class OrderQueryServiceImpl implements OrderQueryService {
         criteriaQuery.select(
                 builder.construct(OrderSummaryOutput.class,
                     root.get("id"),
-                    builder.construct(CustomerMinimalOutput.class,
-                            customer.get("id"),
-                            customer.get("firstName"),
-                            customer.get("lastName"),
-                            customer.get("email"),
-                            customer.get("document"),
-                            customer.get("phone")
-                    ),
                     root.get("totalItems"),
                     root.get("totalAmount"),
                     root.get("placedAt"),
@@ -71,12 +64,25 @@ public class OrderQueryServiceImpl implements OrderQueryService {
                     root.get("cancelAt"),
                     root.get("readyAt"),
                     root.get("status"),
-                    root.get("paymentMethod")
+                    root.get("paymentMethod"),
+                        builder.construct(CustomerMinimalOutput.class,
+                                customer.get("id"),
+                                customer.get("firstName"),
+                                customer.get("lastName"),
+                                customer.get("email"),
+                                customer.get("document"),
+                                customer.get("phone")
+                        )
                 )
         );
 
         Predicate[] predicates = toPredicates(builder, root, filter);
+        Order sortOrder = toSorteOrder(builder, root, filter);
         criteriaQuery.where(predicates);
+        if (sortOrder != null){
+            criteriaQuery.orderBy(sortOrder);
+        }
+
 
         TypedQuery<OrderSummaryOutput> query = entityManager.createQuery(criteriaQuery);
         query.setFirstResult(filter.getSize() * filter.getPage());
@@ -84,6 +90,19 @@ public class OrderQueryServiceImpl implements OrderQueryService {
         PageRequest pageRequest = PageRequest.of(filter.getPage(), filter.getSize());
 
         return new PageImpl<>(query.getResultList(), pageRequest, totalQueryResults);
+    }
+
+    private Order toSorteOrder(CriteriaBuilder builder, Root<OrderPersistenceEntity> root, OrderFilter filter) {
+
+        if (filter.getSortDirectionOrDefault() == Sort.Direction.ASC){
+            return builder.asc(root.get(filter.getSortByPropertyOrDefault().getPropertyName()));
+        }
+
+        if (filter.getSortDirectionOrDefault() == Sort.Direction.DESC){
+            return builder.desc(root.get(filter.getSortByPropertyOrDefault().getPropertyName()));
+        }
+
+        return null;
     }
 
     private Long countTotalQueryResults(OrderFilter filter) {
