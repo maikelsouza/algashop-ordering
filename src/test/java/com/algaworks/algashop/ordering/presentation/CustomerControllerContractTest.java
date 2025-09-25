3ab87878a2da2bdd01d1df558cd83fcfcf25be3a
RE045908 <re045908@qintess.com>
2025-09-25 15:08:13 -0300

    SMS level 3 - 3.3 - Using Mock in Contract Testing

--- src/test/java/com/algaworks/algashop/ordering/presentation/CustomerControllerContractTest.java
+++ src/test/java/com/algaworks/algashop/ordering/presentation/CustomerControllerContractTest.java
@@ -1,23 +1,37 @@
 package com.algaworks.algashop.ordering.presentation;
 
+import com.algaworks.algashop.ordering.application.customer.management.CustomerInput;
+import com.algaworks.algashop.ordering.application.customer.management.CustomerManagementApplicationService;
+import com.algaworks.algashop.ordering.application.customer.query.CustomerOutput;
+import com.algaworks.algashop.ordering.application.customer.query.CustomerOutputTestDataBuilder;
+import com.algaworks.algashop.ordering.application.customer.query.CustomerQueryService;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.nio.charset.StandardCharsets;
+import java.util.UUID;
 
 @WebMvcTest(controllers = CustomerController.class)
 class CustomerControllerContractTest {
 
     @Autowired
     private WebApplicationContext context;
+
+    @MockitoBean
+    private CustomerManagementApplicationService customerManagementApplicationService;
+
+    @MockitoBean
+    private CustomerQueryService customerQueryService;
 
     @BeforeEach
     public void setupAll(){
@@ -28,29 +42,32 @@ class CustomerControllerContractTest {
 
     @Test
     public void createCustomerContract(){
+        CustomerOutput customerOutput = CustomerOutputTestDataBuilder.existing().build();
+        Mockito.when(customerManagementApplicationService.create(Mockito.any(CustomerInput.class)))
+                .thenReturn(UUID.randomUUID());
+        Mockito.when(customerQueryService.findById(Mockito.any(UUID.class)))
+                .thenReturn(customerOutput);
+
         String jsonInput = """
-                {
-                  "id": "380d7e70-7942-4b8d-a0e9-b93d79aa6745",
-                  "firstName": "John",
-                  "lastName": "Doe",
-                  "email": "Johndoe@email.com",
-                  "document": "12345",
-                  "phone": "1191234564",
-                  "birthDate": "1991-07-05",
-                  "registerAt": "2024-04-04T12:45Z",
-                  "archevidAt": "2024-04-04T12:45Z",                  
-                  "promotionNotificationsAllowed": false,
-                  "address": {
-                    "street": "Bourbon Street",
-                    "number": "2000",
-                    "complement": "apt 122",
-                    "neighborhood": "North Ville",
-                    "city": "Yostford",
-                    "state": "South Line",
-                    "zipCode": "12321"
-                  }
-                }
-                """;
+        {
+          "firstName": "John",
+          "lastName": "Doe",
+          "email": "johndoe@email.com",
+          "document": "12345",
+          "phone": "1191234564",
+          "birthDate": "1991-07-05",
+          "promotionNotificationsAllowed": false,
+          "address": {
+            "street": "Bourbon Street",
+            "number": "2000",
+            "complement": "apt 122",
+            "neighborhood": "North Ville",
+            "city": "Yostfort",
+            "state": "South Carolina",
+            "zipCode": "12321"
+          }
+        }
+        """;
         RestAssuredMockMvc
                 .given()
                     .accept(MediaType.APPLICATION_JSON_VALUE)
@@ -63,20 +80,22 @@ class CustomerControllerContractTest {
                     .contentType(MediaType.APPLICATION_JSON_VALUE)
                     .statusCode(HttpStatus.CREATED.value())
                     .body(
-                         "id", Matchers.notNullValue(),
-                    "registeredAt", Matchers.notNullValue(),
+                            "id", Matchers.notNullValue(),
+                            "registeredAt", Matchers.notNullValue(),
                             "firstName", Matchers.is("John"),
                             "lastName", Matchers.is("Doe"),
+                            "email", Matchers.is("johndoe@email.com"),
                             "document", Matchers.is("12345"),
                             "phone", Matchers.is("1191234564"),
                             "birthDate", Matchers.is("1991-07-05"),
                             "promotionNotificationsAllowed", Matchers.is(false),
+                            "loyaltyPoints", Matchers.is(0),
                             "address.street", Matchers.is("Bourbon Street"),
                             "address.number", Matchers.is("2000"),
                             "address.complement", Matchers.is("apt 122"),
                             "address.neighborhood", Matchers.is("North Ville"),
-                            "address.city", Matchers.is("Yostford"),
-                            "address.state", Matchers.is("South Line"),
+                            "address.city", Matchers.is("Yostfort"),
+                            "address.state", Matchers.is("South Carolina"),
                             "address.zipCode", Matchers.is("12321")
                     );
 
