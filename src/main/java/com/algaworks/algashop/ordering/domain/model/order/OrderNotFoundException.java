159bf5e1b3d70ef9978f71a3a9ee729975297fd5
RE045908 <re045908@qintess.com>
2025-10-07 17:18:42 -0300

    SMS level 3 - 4.14 - Simulating Ordering Errors with Mocks

--- src/main/java/com/algaworks/algashop/ordering/domain/model/order/OrderNotFoundException.java
+++ src/main/java/com/algaworks/algashop/ordering/domain/model/order/OrderNotFoundException.java
@@ -1,10 +1,11 @@
 package com.algaworks.algashop.ordering.domain.model.order;
 
+import com.algaworks.algashop.ordering.domain.model.DomainEntityNotFoundException;
 import com.algaworks.algashop.ordering.domain.model.DomainException;
 
 import static com.algaworks.algashop.ordering.domain.model.ErrorMessages.ERROR_ORDER_NOT_FOUND;
 
-public class OrderNotFoundException extends DomainException {
+public class OrderNotFoundException extends DomainEntityNotFoundException {
 
     public OrderNotFoundException(OrderId id) {
         super(String.format(ERROR_ORDER_NOT_FOUND,id));
