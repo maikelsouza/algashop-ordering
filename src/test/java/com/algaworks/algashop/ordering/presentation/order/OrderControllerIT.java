e1e5e932bc086c5a39a3d52003c3f03f24aae3de
RE045908 <re045908@qintess.com>
2025-10-20 10:24:10 -0300

    SMS level 3 - 5.4 - Evaluating response in detail

--- src/test/java/com/algaworks/algashop/ordering/presentation/order/OrderControllerIT.java
+++ src/test/java/com/algaworks/algashop/ordering/presentation/order/OrderControllerIT.java
@@ -1,11 +1,15 @@
 package com.algaworks.algashop.ordering.presentation.order;
 
+import com.algaworks.algashop.ordering.domain.model.order.OrderId;
 import com.algaworks.algashop.ordering.infrastructure.persistence.customer.CustomerPersistenceEntityRepository;
 import com.algaworks.algashop.ordering.infrastructure.persistence.entity.CustomerPersistenceEntityTestDataBuilder;
+import com.algaworks.algashop.ordering.infrastructure.persistence.order.OrderPersistenceEntityRepository;
 import com.algaworks.algashop.ordering.utils.AlgaShopResourceUtils;
 import io.restassured.RestAssured;
 import io.restassured.config.JsonConfig;
 import io.restassured.path.json.config.JsonPathConfig;
+import org.assertj.core.api.Assertions;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +30,9 @@ public class OrderControllerIT {
 
     @Autowired
     private CustomerPersistenceEntityRepository customerRepository;
+
+    @Autowired
+    private OrderPersistenceEntityRepository orderRepository;
 
     private static final UUID validCustomerId = UUID.fromString("6e148bd5-47f6-4022-b9da-07cfaa294f7a");
 
@@ -54,7 +61,7 @@ public class OrderControllerIT {
     @Test
     public void shouldCreateOrderUsingProduct(){
         String json = AlgaShopResourceUtils.readContent("json/create-order-with-product.json");
-        RestAssured
+        String createdOrderId = RestAssured
                 .given()
                     .accept(MediaType.APPLICATION_JSON_VALUE)
                     .contentType("application/vnd.order-with-product.v1+json")
@@ -64,7 +71,14 @@ public class OrderControllerIT {
                 .then()
                     .assertThat()
                     .contentType(MediaType.APPLICATION_JSON_VALUE)
-                    .statusCode(HttpStatus.CREATED.value());
+                    .statusCode(HttpStatus.CREATED.value())
+                    .body("id", Matchers.not(Matchers.emptyString()),
+                            "customer.id", Matchers.is(validCustomerId.toString()))
+                    .extract().jsonPath().getString("id");
+
+        boolean orderExists = orderRepository.existsById(new OrderId(createdOrderId).value().toLong());
+
+        Assertions.assertThat(orderExists).isTrue();
     }
 
     @Test
