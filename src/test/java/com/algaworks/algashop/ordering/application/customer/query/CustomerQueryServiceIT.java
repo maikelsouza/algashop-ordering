d63f1fb3d5a3784b72aae19ab3519f5388f34f6f
RE045908 <re045908@qintess.com>
2025-10-28 13:13:06 -0300

    SMS level 3 - 6.11 - Cleaning up test data with @Sql

--- src/test/java/com/algaworks/algashop/ordering/application/customer/query/CustomerQueryServiceIT.java
+++ src/test/java/com/algaworks/algashop/ordering/application/customer/query/CustomerQueryServiceIT.java
@@ -6,15 +6,19 @@ import com.algaworks.algashop.ordering.domain.model.customer.CustomerId;
 import com.algaworks.algashop.ordering.domain.model.customer.CustomerTestDataBuilder;
 import com.algaworks.algashop.ordering.domain.model.customer.Customers;
 import org.assertj.core.api.Assertions;
+import org.hibernate.annotations.processing.SQL;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Sql(scripts = "classpath:sql/clean-database.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS)
+@Sql(scripts = "classpath:sql/clean-database.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 @Transactional
 class CustomerQueryServiceIT {
 
