04e3316b6c2da466b77957c57975cde146a9c110
RE045908 <re045908@qintess.com>
2025-11-18 14:33:17 -0300

    SMS level 3 - 10.5 - Adding Testcontainer to presentation tests

--- src/test/java/com/algaworks/algashop/ordering/presentation/shoppingcart/ShoppingCartControllerIT.java
+++ src/test/java/com/algaworks/algashop/ordering/presentation/shoppingcart/ShoppingCartControllerIT.java
@@ -1,40 +1,22 @@
 package com.algaworks.algashop.ordering.presentation.shoppingcart;
 
 import com.algaworks.algashop.ordering.infrastructure.persistence.customer.CustomerPersistenceEntityRepository;
-import com.algaworks.algashop.ordering.infrastructure.persistence.entity.CustomerPersistenceEntityTestDataBuilder;
-import com.algaworks.algashop.ordering.infrastructure.persistence.entity.ShoppingCartPersistenceEntityTestDataBuilder;
 import com.algaworks.algashop.ordering.infrastructure.persistence.shoppingcart.ShoppingCartPersistenceEntity;
 import com.algaworks.algashop.ordering.infrastructure.persistence.shoppingcart.ShoppingCartPersistenceEntityRepository;
+import com.algaworks.algashop.ordering.presentation.AbstractPresentationIT;
 import com.algaworks.algashop.ordering.utils.AlgaShopResourceUtils;
-import com.github.tomakehurst.wiremock.WireMockServer;
-import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer;
 import io.restassured.RestAssured;
-import io.restassured.config.JsonConfig;
-import io.restassured.path.json.config.JsonPathConfig;
 import org.assertj.core.api.Assertions;
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.jdbc.Sql;
 
-import java.util.HashSet;
 import java.util.UUID;
 
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Sql(scripts = "classpath:db/testdata/afterMigrate.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS)
-@Sql(scripts = "classpath:db/clean/afterMigrate.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_CLASS)
-public class ShoppingCartControllerIT {
-
-    @LocalServerPort
-    private int port;
+public class ShoppingCartControllerIT extends AbstractPresentationIT {
 
     @Autowired
     private CustomerPersistenceEntityRepository customerRepository;
@@ -48,38 +30,22 @@ public class ShoppingCartControllerIT {
 
     private static final UUID invalidShoppingCartId = UUID.fromString("a7f4c8b9-2d35-4871-9e3c-4b62f1a9d7e5");
 
-    private WireMockServer wireMockProductCatalog;
 
-    private WireMockServer wireMockRapidex;
 
     @BeforeEach
     public void setup(){
-        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-        RestAssured.port = port;
-        JsonConfig jsonConfig = JsonConfig.jsonConfig()
-                .numberReturnType(JsonPathConfig.NumberReturnType.BIG_DECIMAL);
-        RestAssured.config().jsonConfig(jsonConfig);
-
-        wireMockProductCatalog = new WireMockServer(
-                options()
-                        .port(8781)
-                        .usingFilesUnderDirectory("src/test/resources/wiremock/product-catalog")
-                        .extensions(new ResponseTemplateTransformer(true)));
-
-        wireMockRapidex = new WireMockServer(
-                options()
-                        .port(8780)
-                        .usingFilesUnderDirectory("src/test/resources/wiremock/rapidex")
-                        .extensions(new ResponseTemplateTransformer(true)));
-
-        wireMockProductCatalog.start();
-        wireMockRapidex.start();
+        super.beforeEach();
     }
 
-    @AfterEach
-    public void after(){
-        wireMockProductCatalog.stop();
-        wireMockRapidex.stop();
+    @BeforeAll
+    public static void setupBeforeAll(){
+        AbstractPresentationIT.initWireMock();
+    }
+
+
+    @AfterAll
+    public static void afterAll(){
+        AbstractPresentationIT.stopMock();
     }
 
     @Test
