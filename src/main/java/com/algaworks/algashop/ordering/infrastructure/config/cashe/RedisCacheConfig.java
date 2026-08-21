8ed175f86b662af45eb4c445026f2c0c2d0255a6
RE045908 <re045908@qintess.com>
2026-08-21 08:58:21 -0300

    SMS level 4 - 9.15 - Client-side cache and cache-aside

---
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/config/cashe/RedisCacheConfig.java
@@ -0,0 +1,30 @@
+package com.algaworks.algashop.ordering.infrastructure.config.cashe;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.cache.autoconfigure.RedisCacheManagerBuilderCustomizer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+@ConditionalOnProperty(name = "spring.cache.type", havingValue = "redis")
+public class RedisCacheConfig {
+
+
+    @Bean
+    public RedisCacheManagerBuilderCustomizer redisCacheManagerCustomizer() {
+        RedisCacheConfiguration defaultCacheConfig = RedisCacheConfiguration
+                .defaultCacheConfig()
+                .computePrefixWith(c-> c + ":")
+                .entryTtl(Duration.ofMinutes(1));
+        return (builder) -> builder.cacheDefaults(defaultCacheConfig)
+                .withCacheConfiguration("algashop:products-catalog-api:v1",
+                        defaultCacheConfig.disableCachingNullValues().entryTtl(Duration.ofMinutes(5)));
+    }
+
+
+}
