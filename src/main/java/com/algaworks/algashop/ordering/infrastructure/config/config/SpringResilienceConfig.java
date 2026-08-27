08dbf2870bd8064d0ff723c142596571be614df6
RE045908 <re045908@qintess.com>
2026-08-27 07:50:25 -0300

    SMS level 4 - 10.6 - Using the Retry Pattern with @Retryable

---
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/config/config/SpringResilienceConfig.java
@@ -0,0 +1,9 @@
+package com.algaworks.algashop.ordering.infrastructure.config.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.resilience.annotation.EnableResilientMethods;
+
+@Configuration
+@EnableResilientMethods
+public class SpringResilienceConfig {
+}
--- src/main/java/com/algaworks/algashop/ordering/infrastructure/product/client/http/ProductCatalogServiceHttpImpl.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/product/client/http/ProductCatalogServiceHttpImpl.java
@@ -8,6 +8,8 @@ import com.algaworks.algashop.ordering.core.domain.model.product.ProductName;
 import com.algaworks.algashop.ordering.presentation.BadGatewayException;
 import com.algaworks.algashop.ordering.presentation.GatewayTimeoutException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.resilience.annotation.Retryable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.ResourceAccessException;
@@ -17,13 +19,20 @@ import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class ProductCatalogServiceHttpImpl implements ProductCatalogService {
 
     private final ProductCatalogAPIClient productCatalogAPIClient;
 
+    @Retryable(
+            maxRetries = 3,
+            delayString ="3s",
+            multiplier = 2,
+            includes = {GatewayTimeoutException.class, BadGatewayException.class})
     @Override
     public Optional<Product> ofId(ProductId productId) {
         ProductResponse productResponse;
+        log.info("Loading product {}", productId);
         try {
             productResponse = productCatalogAPIClient.getById(productId.value());
         } catch (ResourceAccessException e){
