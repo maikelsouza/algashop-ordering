4ec62257409c7fd0aa55462c370a41b9431e4780
RE045908 <re045908@qintess.com>
2026-09-19 12:46:27 -0300

    SMS level 4 - 10.17 - Shipping rate lookup endpoint

---
+++ src/main/java/com/algaworks/algashop/ordering/core/application/shipping/ShippingCostPreviewInput.java
@@ -0,0 +1,13 @@
+package com.algaworks.algashop.ordering.core.application.shipping;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class ShippingCostPreviewInput {
+
+    @NotBlank
+    @Size(min = 5, max = 5)
+    private String zipCode;
+}
